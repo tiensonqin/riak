1b0095888d69220d3f74250c424013d2cec421ad
Tienson Qin <tiensonqin@gmail.com>
2014-09-25 18:31:08 +0800

    remove riak_search and yz

--- apps/riak/src/chash_keyfun.erl
+++ apps/riak/src/chash_keyfun.erl
@@ -3,5 +3,11 @@
 -export([chash_index_keyfun/1]).
 
 chash_index_keyfun({Bucket, Key}) ->
-    [IndexPartOfKey, _Id] = string:tokens(binary_to_list(Key), "$$"),
+    IndexPartOfKey = case string:tokens(binary_to_list(Key), "$$") of
+                         [Key0, _Id] ->
+                             Key0;
+                         [Key0] ->
+                             Key0
+                     end,
+
     chash:key_of({Bucket, list_to_binary(IndexPartOfKey)}).
